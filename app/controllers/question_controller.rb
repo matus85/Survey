0a83364cd82b7d2c2466f3a1880030528b21fe2d
mike <matus85@gmail.com>
2011-04-23 19:14:16 -0500

    update

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -5,12 +5,17 @@ class QuestionController < ApplicationController
   end
   
   def new
+    @survey = Survey.find(params[:id])
     @question = Question.new
   end
     def create
     @questions = Question.new(params[:question])
+    @survey = Survey.find(params[:id])
+    
+    @survey.questions << @questions
+    
     if(@questions.save)
-      redirect_to(:action =>'list')
+      redirect_to(:controller => 'survey', :action =>'show', :id => @survey.id)
     else
       render('new')
     end 
